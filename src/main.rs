77c62ce1be67f0bdef6ecc0ab4c2ac5a50c21a88
Frits Sweijen <tikk3r@users.noreply.github.com>
2024-06-19 14:50:09 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use clap::Parser;
 use fitsio::images::{ImageDescription, ImageType};
 use fitsio::FitsFile;
 use fitsrs::fits::Fits;
+use fitsrs::hdu::HDU;
+use fitsrs::hdu::header::extension::Xtension;
+use fitsrs::hdu::header::extension::image::Image;
+use fitsrs::hdu::header::Header;
+use fitsio_sys;
 use ndarray::{s, Array, ArrayD, Axis};
 use wcs::{LonLat, WCS};
 
@@ -33,22 +38,18 @@ struct Args {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let f = File::open(
-        &args.fitsimage,
-    )
-    .unwrap();
-    // Use BufReader here to avoid reading the entire file in memory.
-    // We just want quick access to the header at this point.
+    let f = File::open(&args.fitsimage).unwrap();
     let mut reader = BufReader::new(f);
-    let Fits { hdu } = Fits::from_reader(&mut reader).unwrap();
-    let header = hdu.get_header();
+    // Somehow this is needed. Skips the first card?
+    let _ = reader.seek_relative(80);
+    let header = Header::parse_header(&mut reader)?;
     let wcs = WCS::new(&header).unwrap();
     let coord = LonLat::new(args.ra.to_radians(), args.dec.to_radians());
     let coord_pix = wcs.proj_lonlat(&coord).unwrap();
     println!(
         "Centring cutout on (x, y) = ({}, {})",
-        coord_pix.x() as usize,
-        coord_pix.y() as usize
+        coord_pix.x() as u64,
+        coord_pix.y() as u64,
     );
 
     let mut fptr = FitsFile::open(&args.fitsimage)?;
@@ -56,15 +57,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cdelt1: f64 = hdu.read_key(&mut fptr, "CDELT1").unwrap();
     let imsize: usize = (args.size / cdelt1.abs()).ceil() as usize;
     println!("New image size: ({} x {})", imsize, imsize);
-    let mut _data: ArrayD<f32> = hdu.read_image(&mut fptr)?;
-    // Assume RA and DEC axes are always the last two axes.
-    while _data.shape().len() > 2 {
-        _data = _data.remove_axis(Axis(0));
-    }
     let rrange = coord_pix.y() as usize + 1 - imsize / 2..coord_pix.y() as usize + imsize / 2 + 1;
     let crange = coord_pix.x() as usize + 1 - imsize / 2..coord_pix.x() as usize + imsize / 2 + 1;
-    let _cutout = _data.slice(s![rrange, crange]);
-
     let img_desc = ImageDescription {
         data_type: ImageType::Float,
         dimensions: &[imsize, imsize],
@@ -72,6 +66,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut fptr_new = FitsFile::create("output.fits")
         .with_custom_primary(&img_desc)
         .open()?;
+
     hdu.write_key(&mut fptr_new, "CRVAL1", args.ra)?;
     hdu.write_key(&mut fptr_new, "CRVAL2", args.dec)?;
 
@@ -87,17 +82,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ctype2: std::string::String = hdu.read_key(&mut fptr, "CTYPE2").unwrap();
     hdu.write_key(&mut fptr_new, "CTYPE2", ctype2)?;
 
+    let ctype3: std::string::String = hdu.read_key(&mut fptr, "CTYPE3").unwrap();
+    hdu.write_key(&mut fptr_new, "CTYPE3", ctype3)?;
+
+    let ctype4: std::string::String = hdu.read_key(&mut fptr, "CTYPE4").unwrap();
+    hdu.write_key(&mut fptr_new, "CTYPE4", ctype4)?;
+
     let radesys: std::string::String = hdu.read_key(&mut fptr, "RADESYS").unwrap();
     hdu.write_key(&mut fptr_new, "RADESYS", radesys)?;
-    let lonpole: std::string::String = hdu.read_key(&mut fptr, "LONPOLE").unwrap();
+    let lonpole: f64 = hdu.read_key(&mut fptr, "LONPOLE").unwrap();
     hdu.write_key(&mut fptr_new, "LONPOLE", lonpole)?;
-    let latpole: std::string::String = hdu.read_key(&mut fptr, "LATPOLE").unwrap();
+    let latpole: f64 = hdu.read_key(&mut fptr, "LATPOLE").unwrap();
     hdu.write_key(&mut fptr_new, "LATPOLE", latpole)?;
 
-    hdu.write_key(&mut fptr_new, "NAXIS1", imsize as u64)?;
-    hdu.write_key(&mut fptr_new, "NAXIS2", imsize as u64)?;
+    // Only works for the "4D" LOFAR images at the moment.
+    let zcoord = 0..1;
+    let wcoord = 0..1;
+    let cutout_flat: Vec<f64> = hdu.read_region(&mut fptr, &[&rrange, &crange, &zcoord, &wcoord])?;
+    dbg!(cutout_flat.len());
 
-    let cutout_flat = Array::from_iter(_cutout.iter().cloned()).to_vec();
     hdu.write_region(&mut fptr_new, &[&(0..imsize), &(0..imsize)], &cutout_flat)?;
     Ok(())
 }
